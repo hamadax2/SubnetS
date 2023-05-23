392335fb2809c8d30789812b820ee77e4c59a5ab
hamadax2 <44388599+hamadax2@users.noreply.github.com>
2023-05-23 11:27:00 +0200

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,26 @@
+import ipaddress
+import os
+str= """\033[91m
+         👑 ━─━  ☚➳нɑм亗ɑdɑ☛  ━─━ 👑
+           
+         ☛ Join Telegram ¬ t.me/klinuxsd
+
+"""
+os.system('clear')
+os.system('figlet Subnet-S')
+print(str)
+
+m = input('ENTER SUBDOMAIN EX.127.0.0.1/24 :: ')
+with open('ip.txt','w') as f:
+	for ip in ipaddress.IPv4Network(m):
+		f.write(str(ip)+'\n')
+os.system('clear')		
+port = input('Which port you want to scann 443 or 80 ?\n')
+if port == '80':
+    os.system('bugscanner ip.txt --port 80 --method get --o hosts.txt')
+elif port == '443':
+   os.system('bugscanner ip.txt --port 443 --method get --o hosts.txt')
+print('Saved in hosts.txt')   
+    
+		
+		
