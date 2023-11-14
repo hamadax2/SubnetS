da54910c30291c765b5e3a6ba041c09263efdab3
Mohamed Sulieman Alomda <44388599+hamadax2@users.noreply.github.com>
2023-11-14 09:40:18 +0200

    Update subnet.py

--- subnet.py
+++ subnet.py
@@ -2,7 +2,7 @@ import ipaddress
 import os
 xstr= """\033[91m
          👑 ━─━  ☚➳нɑм亗ɑdɑ☛  ━─━ 👑
-           
+
          ☛ Join Telegram ¬ t.me/klinuxsd
 
 """
@@ -12,15 +12,10 @@ print(xstr)
 
 m = input('ENTER SUBDOMAIN EX.127.0.0.1/24 :: ')
 with open('ip.txt','w') as f:
-	for xip in ipaddress.IPv4Network(m):
-		f.write(str(xip)+'\n')
-os.system('clear')		
-port = input('Which port you want to scann 443 or 80 ?\n')
-if port == '80':
-    os.system('bugscanner ip.txt --port 80 --method get --o hosts.txt')
-elif port == '443':
-   os.system('bugscanner ip.txt --port 443 --method get --o hosts.txt')
-print('Saved in hosts.txt')   
-    
-		
-		
+        for xip in ipaddress.IPv4Network(m):
+                f.write(str(xip)+'\n')
+os.system('clear')
+port = input('Which port you want to scann ?\n')
+os.system(f'bugscanner ip.txt --port {port} --method get --o hosts.txt')
+print('Saved in hosts.txt')
+os.system("cat hosts.txt")
